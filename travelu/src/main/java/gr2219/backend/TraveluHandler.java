af26c499ad16bf7fd5e6fa79070877ad1b746431
Torbjørn Wiik <torbjw@stud.ntnu.no>
2022-09-22 11:57:44 +0200

    fix: read/write to file

--- travelu/src/main/java/gr2219/backend/TraveluHandler.java
+++ travelu/src/main/java/gr2219/backend/TraveluHandler.java
@@ -1,6 +1,7 @@
 package gr2219.backend;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -11,18 +12,22 @@ import com.google.gson.GsonBuilder;
 
 public class TraveluHandler {
 
+    private File getFile() {
+        return new File(TraveluHandler.class.getResource("data/").getFile() + "persistence.json");
+    }
+
     public void writeJSON(DestinationList log) throws IOException {
         GsonBuilder builder = new GsonBuilder();
         builder.setPrettyPrinting().serializeNulls();
         Gson gson = builder.create();
-        FileWriter writer = new FileWriter("travelu/persistence.json");
+        FileWriter writer = new FileWriter(getFile());
         writer.write(gson.toJson(log));
         writer.close();
     }
 
     public DestinationList readJSON() throws FileNotFoundException {
         Gson gson = new Gson();
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("travelu/persistence.json"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(getFile()));
         DestinationList log = gson.fromJson(bufferedReader, DestinationList.class);
         return log;
     }
