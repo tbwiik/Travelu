db7cd69c7ca754cc5711baef23f8bb7f59f20b74
John Hausberg Anfindsen <johnhanf@stud.ntnu.no>
2022-09-22 11:17:36 +0200

    Resolve "Move logic from PrimaryController to DestinationList and Destination"

--- travelu/src/main/java/gr2219/backend/DestinationList.java
+++ travelu/src/main/java/gr2219/backend/DestinationList.java
@@ -29,4 +29,22 @@ public class DestinationList {
         return destinations;
     }
 
+    public List<String> getDestinationNames(){
+        List<String> destinationNames = new ArrayList<>();
+        getList().forEach(destination -> destinationNames.add(destination.getName()));
+
+        return destinationNames;
+    }
+
+    // Used to search for duplicates
+    private List<String> getLowerCaseDestinationNames(){
+        List<String> destinationNames = new ArrayList<>();
+        getDestinationNames().forEach(destinationName -> destinationNames.add(destinationName.toLowerCase()));
+        return destinationNames;
+    }
+
+    public boolean containsDestination(String destinationName){
+        return getLowerCaseDestinationNames().contains(destinationName);
+    }
+
 }
