90a2e668237a38a33642a0e4be9ee1a5fef9a605
John Hausberg Anfindsen <johnhanf@stud.ntnu.no>
2022-09-30 10:19:13 +0200

    fix: change package declaration in fxui

--- travelu/fxui/src/main/java/travelu/fxui/DestinationController.java
+++ travelu/fxui/src/main/java/travelu/fxui/DestinationController.java
@@ -1,4 +1,4 @@
-package app;
+package travelu.fxui;
 
 import java.io.IOException;
 
--- travelu/fxui/src/main/java/travelu/fxui/DestinationListController.java
+++ travelu/fxui/src/main/java/travelu/fxui/DestinationListController.java
@@ -1,4 +1,4 @@
-package app;
+package travelu.fxui;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
