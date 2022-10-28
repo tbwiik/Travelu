1e0bd5288f093b8089370d7f9b90e863a14eaa0d
John Hausberg Anfindsen <johnhanf@stud.ntnu.no>
2022-10-28 11:11:09 +0200

    feat: change structure of DateInterval, and implement validation

--- travelu/core/src/main/java/travelu/core/DateInterval.java
+++ travelu/core/src/main/java/travelu/core/DateInterval.java
@@ -1,13 +1,16 @@
 package travelu.core;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Date-interval, from-to, because problems with filewriting using pair and
  * date-object
  */
 public class DateInterval {
 
-    private int[] startDate;
-    private int[] endDate;
+    private String startDate;
+    private String endDate;
 
     /**
      * Create DateInterval with two int-arrays
@@ -15,26 +18,16 @@ public class DateInterval {
      * @param startDate containing day, month, year
      * @param endDate   containing day, month, year
      */
-    public DateInterval(int[] startDate, int[] endDate) {
-        this.startDate = startDate.clone();
-        this.endDate = endDate.clone();
-    }
+    public DateInterval(String startDate, String endDate) {
+        if (isValidDate(startDate)) {
+            this.startDate = startDate;
+        } else
+            throw new IllegalArgumentException("Invalid startDate");
 
-    /**
-     * Create DateInterval with every value specified
-     * 
-     * @param startDay
-     * @param startMonth
-     * @param startYear
-     * @param endDay
-     * @param endMonth
-     * @param endYear
-     */
-    public DateInterval(int startDay, int startMonth, int startYear, int endDay, int endMonth, int endYear) {
-
-        startDate = new int[] { startDay, startMonth, startYear };
-        endDate = new int[] { endDay, endMonth, endYear };
-
+        if (isValidDate(endDate)) {
+            this.endDate = endDate;
+        } else
+            throw new IllegalArgumentException("Invalid endDate");
     }
 
     /**
@@ -50,20 +43,44 @@ public class DateInterval {
         this.endDate = dateInterval.getEndDate();
     }
 
-    public int[] getStartDate() {
-        return startDate.clone();
+    public String getStartDate() {
+        return this.startDate;
     }
 
-    public int[] getEndDate() {
-        return endDate.clone();
+    public String getEndDate() {
+        return this.endDate;
     }
 
-    public void setStartDate(int[] startDate) {
-        this.startDate = startDate.clone();
+    public void setStartDate(String startDate) {
+        if (isValidDate(startDate))
+            this.startDate = startDate;
+        else
+            throw new IllegalArgumentException("invalid start date");
     }
 
-    public void setEndDate(int[] endDate) {
-        this.endDate = endDate.clone();
+    public void setEndDate(String endDate) {
+        if (isValidDate(endDate))
+            this.endDate = endDate;
+        else
+            throw new IllegalArgumentException("invalid start date");
     }
+
+    private boolean isValidDate(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        
+        // We allow the dates to be null
+        if (dateString == null) {
+            return true;
+        }
+
+    try{
+        LocalDate.parse(dateString, formatter);
+    }catch(
+    Exception e){
+        return false;
+    }
+    
+    return true;
+}
 
 }
