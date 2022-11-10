7bd44b34c677d34680f9c6c036b02c69bca3884e
John Hausberg Anfindsen <johnhanf@stud.ntnu.no>
2022-11-10 13:04:36 +0100

    test: update DateIntervalTest with new date format and exceptions

--- travelu/core/src/test/java/travelu/core/DateIntervalTest.java
+++ travelu/core/src/test/java/travelu/core/DateIntervalTest.java
@@ -49,31 +49,31 @@ public class DateIntervalTest {
         dateInterval1.setArrivalDate("01/01/2019");
         assertEquals("01/01/2019", dateInterval1.getArrivalDate());
 
-        dateInterval1.setDepartureDate("01/04/2019");
-        assertEquals("01/04/2019", dateInterval1.getDepartureDate());
+        dateInterval1.setDepartureDate("04/01/2019");
+        assertEquals("04/01/2019", dateInterval1.getDepartureDate());
 
         dateInterval2.setArrivalDate("01/01/2019");
         assertEquals("01/01/2019", dateInterval2.getArrivalDate());
 
-        dateInterval2.setDepartureDate("01/04/2019");
-        assertEquals("01/04/2019", dateInterval2.getDepartureDate());
+        dateInterval2.setDepartureDate("04/01/2019");
+        assertEquals("04/01/2019", dateInterval2.getDepartureDate());
 
         // check if date interval stays unchanged on invalid date input
-        assertThrows(IllegalArgumentException.class, () -> dateInterval1.setArrivalDate("15/02/2019"));
+        assertThrows(IllegalArgumentException.class, () -> dateInterval1.setArrivalDate("02/15/2019"));
         assertEquals("01/01/2019", dateInterval1.getArrivalDate());
 
-        assertThrows(IllegalArgumentException.class, () -> dateInterval2.setDepartureDate("01/32/2019"));
-        assertEquals("01/04/2019", dateInterval2.getDepartureDate());
+        assertThrows(IllegalArgumentException.class, () -> dateInterval2.setDepartureDate("32/01/2019"));
+        assertEquals("04/01/2019", dateInterval2.getDepartureDate());
 
         // check if date interval stays unchanged when arrival date is after departure
         // date
-        assertThrows(IllegalArgumentException.class, () -> dateInterval1.setArrivalDate("01/01/2022"));
+        assertThrows(IllegalStateException.class, () -> dateInterval1.setArrivalDate("01/01/2022"));
         assertEquals("01/01/2019", dateInterval1.getArrivalDate());
 
         // check if date interval stays unchanged when departure date is before arrival
         // date
-        assertThrows(IllegalArgumentException.class, () -> dateInterval2.setDepartureDate("01/01/2018"));
-        assertEquals("01/04/2019", dateInterval2.getDepartureDate());
+        assertThrows(IllegalStateException.class, () -> dateInterval2.setDepartureDate("01/01/2018"));
+        assertEquals("04/01/2019", dateInterval2.getDepartureDate());
 
     }
 
