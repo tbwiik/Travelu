3556f5030b3d9d56cdda450bfabd8cf826acbb29
Torbjørn Wiik <torbjw@stud.ntnu.no>
2022-11-13 13:22:44 +0100

    docs: add documentation of client

--- travelu/client/src/main/java/travelu/client/Client.java
+++ travelu/client/src/main/java/travelu/client/Client.java
@@ -18,22 +18,24 @@ public class Client {
     private final String serverUrl;
     private final int serverPort;
 
+    /**
+     * Initialize client used by ui for server communications
+     * 
+     * @param serverUrl  URL where server is hosted
+     * @param serverPort Port where server is hosted
+     */
     public Client(String serverUrl, int serverPort) {
         this.serverUrl = serverUrl;
         this.serverPort = serverPort;
     }
 
-    public DestinationList getDestinationList() throws URISyntaxException, InterruptedException, ExecutionException {
-
-        HttpResponse<String> response = this.get("/api/v1/entries/" + "destinationlist");
-
-        Gson gson = new Gson();
-
-        DestinationList result = gson.fromJson(response.body(), DestinationList.class);
-
-        return result;
-    }
-
+    /**
+     * Asynchronous get request
+     * 
+     * @param endpoint where the request is sent to
+     * @return the HTTP async response
+     * @throws URISyntaxException if invalid URI
+     */
     private CompletableFuture<HttpResponse<String>> getAsync(String endpoint) throws URISyntaxException {
         HttpClient client = HttpClient.newBuilder().build();
 
@@ -43,15 +45,46 @@ public class Client {
                 .build();
 
         return client.sendAsync(request, BodyHandlers.ofString());
-
     }
 
+    /**
+     * Synchronous get request
+     * <p>
+     * Makes use of {@link #getAsync(String)}
+     * 
+     * @param endpoint where the request is sent to
+     * @return the HTTP response
+     * @throws URISyntaxException   if invalid URI
+     * @throws InterruptedException if interruption occurs during retrieval of
+     *                              response
+     * @throws ExecutionException
+     */
     private HttpResponse<String> get(final String endpoint)
             throws URISyntaxException, InterruptedException, ExecutionException {
 
         HttpResponse<String> response = this.getAsync(endpoint).get();
 
         return response;
+    }
+
+    /**
+     * Get a {@link DestinationList} from the server
+     * 
+     * @return {@link DestinationList} object from server
+     * @throws URISyntaxException   if invalid URI
+     * @throws InterruptedException if interruption occurs during retrival of
+     *                              response
+     * @throws ExecutionException
+     */
+    public DestinationList getDestinationList() throws URISyntaxException, InterruptedException, ExecutionException {
+
+        HttpResponse<String> response = this.get("/api/v1/entries/" + "destinationlist");
+
+        Gson gson = new Gson();
+
+        DestinationList result = gson.fromJson(response.body(), DestinationList.class);
+
+        return result;
     }
 
 }
