13041d8a405ce22cf232dd6377286262b1686ed7
Torbjørn Wiik <torbjw@stud.ntnu.no>
2022-11-16 14:33:51 +0100

    fix: handle removing unnexisting item through server

--- travelu/restserver/src/main/java/travelu/restserver/TraveluController.java
+++ travelu/restserver/src/main/java/travelu/restserver/TraveluController.java
@@ -106,15 +106,8 @@ public class TraveluController {
      * @param destinationJSON
      */
     @PostMapping(value = "/remove", produces = "application/json")
-    public void removeDestinationJSON(final @RequestBody String destinationName) {
-        Gson gson = new Gson();
-        String destination = gson.fromJson(destinationName, String.class);
-        try {
-            traveluService.getDestinationList().removeDestination(destination);
-            traveluService.save();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void removeDestinationJSON(final @RequestBody String destinationName) throws NoSuchElementException {
+        traveluService.getDestinationList().removeDestination(destinationName);
     }
 
     /**
