722897e9f5559f51379010be268ed27fff9242b1
Torbjørn Wiik <torbjw@stud.ntnu.no>
2022-11-18 16:26:18 +0100

    refactor: seperate out httprequests (1)

--- travelu/client/src/main/java/travelu/client/Client.java
+++ travelu/client/src/main/java/travelu/client/Client.java
@@ -1,27 +1,27 @@
 package travelu.client;
 
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.concurrent.CompletableFuture;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.regex.Pattern;
 
 import com.google.gson.Gson;
 
 import travelu.core.DestinationList;
 import travelu.core.Destination;
 
+/**
+ * Handle api-requests to server
+ */
 public class Client {
 
     private final static String API_ADDRESS = "/api/v1/entries/";
-    private final static String HTTP_STATUS_OK = "[2][0-9]*";
 
-    private final String serverUrl;
-    private final int serverPort;
+    /**
+     * Handle generic Http-request
+     */
+    private final Requests httpRequests;
 
     /**
      * Initialize client used by ui for server communications
@@ -29,53 +29,8 @@ public class Client {
      * @param serverUrl  URL where server is hosted
      * @param serverPort Port where server is hosted
      */
-    public Client(String serverUrl, int serverPort) {
-        this.serverUrl = serverUrl;
-        this.serverPort = serverPort;
-    }
-
-    /**
-     * Asynchronous get request
-     * <p>
-     * Method written per {@link HttpRequest} documentation
-     * 
-     * @param endpoint where the request is sent to
-     * @return the HTTP async response
-     * @throws URISyntaxException if invalid URI
-     */
-    private CompletableFuture<HttpResponse<String>> getAsync(String endpoint) throws URISyntaxException {
-        HttpClient client = HttpClient.newBuilder().build();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(new URI(this.serverUrl + ":" + this.serverPort + endpoint))
-                .build();
-
-        return client.sendAsync(request, BodyHandlers.ofString());
-    }
-
-    /**
-     * Synchronous get request
-     * <p>
-     * Makes use of {@link #getAsync(String)}
-     * 
-     * @param endpoint where the request is sent to
-     * @return the HTTP response
-     * @throws URISyntaxException   if invalid URI
-     * @throws InterruptedException if interruption occurs during retrieval of
-     *                              response
-     * @throws ExecutionException
-     * @throws ServerException      if http request not successfull
-     */
-    private HttpResponse<String> get(String endpoint)
-            throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
-
-        HttpResponse<String> httpResponse = this.getAsync(endpoint).get();
-
-        if (!Pattern.matches(HTTP_STATUS_OK, String.valueOf(httpResponse.statusCode())))
-            throw new ServerException("A server error occured", httpResponse.statusCode());
-
-        return this.getAsync(endpoint).get();
+    public Client(final String serverUrl, final int serverPort) {
+        httpRequests = new Requests(serverUrl, serverPort);
     }
 
     /**
@@ -91,7 +46,7 @@ public class Client {
     public DestinationList getDestinationList()
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
 
-        HttpResponse<String> response = this.get(API_ADDRESS + "destinationlist");
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "destinationlist");
 
         Gson gson = new Gson();
 
@@ -116,7 +71,7 @@ public class Client {
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
 
         String fixedDestinationName = destinationName.replace(" ", "%20");
-        HttpResponse<String> response = this.get(API_ADDRESS + fixedDestinationName);
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + fixedDestinationName);
 
         Gson gson = new Gson();
 
@@ -127,8 +82,6 @@ public class Client {
 
     /**
      * Get current chosen {@link Destination} from the server
-     * <p>
-     * First send a get-request to get name
      * <p>
      * Then use {@link #getDestination(String)} to get Destination
      * 
@@ -141,57 +94,66 @@ public class Client {
     public Destination getCurrentDestination()
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
 
-        HttpResponse<String> response = this.get(API_ADDRESS + "currentDestination");
-
-        Destination destination = getDestination(response.body());
+        Destination destination = getDestination(getCurrentDestinationName());
 
         return destination;
     }
 
     /**
-     * Asynchronous post request
-     * <p>
-     * Method written per {@link HttpRequest} documentation
+     * Get name of chosen {@link Destination} from the server
      * 
-     * @param endpoint where the request is sent to
-     * @return the HTTP async response
-     * @throws URISyntaxException if invalid URI
-     */
-    private CompletableFuture<HttpResponse<String>> postAsync(String endpoint, String payload)
-            throws URISyntaxException {
-
-        HttpClient client = HttpClient.newBuilder().build();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(payload))
-                .uri(new URI(this.serverUrl + ":" + this.serverPort + endpoint))
-                .build();
-
-        return client.sendAsync(request, BodyHandlers.ofString());
-    }
-
-    /**
-     * Synchronous post request
-     * <p>
-     * Makes use of {@link #postAsync(String)}
-     * 
-     * @param endpoint where the request is sent to
-     * @return the HTTP response
-     * @throws URISyntaxException   if invalid URI
-     * @throws InterruptedException if interruption occurs during retrieval of
-     *                              response
+     * @return name of chosen {@link Destination}
+     * @throws URISyntaxException
+     * @throws InterruptedException
      * @throws ExecutionException
      * @throws ServerException      if http request not successfull
      */
-    private HttpResponse<String> post(String endpoint, String payload)
+    public String getCurrentDestinationName()
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
 
-        HttpResponse<String> httpResponse = this.postAsync(endpoint, payload).get();
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "currentDestination");
+        String result = response.body().replace("%20", " ");
 
-        if (!Pattern.matches(HTTP_STATUS_OK, String.valueOf(httpResponse.statusCode())))
-            throw new ServerException("A server error occured", httpResponse.statusCode());
+        return result;
+    }
 
-        return httpResponse;
+    /**
+     * Get names of stored Destinations
+     * 
+     * @return destination names
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws ServerException      if http request not successfull
+     */
+    public List<String> getDestinationNames()
+            throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
+
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "destinationNames");
+
+        String[] names = new Gson().fromJson(response.body(), String[].class);
+
+        return Arrays.asList(names);
+    }
+
+    /**
+     * Check if name is already present in list
+     * 
+     * @return true if in list
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws ServerException      if http request not successfull
+     */
+    public boolean containsDestination(final String destinationName)
+            throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
+
+        String destinationNameJSON = destinationName.replace(" ", "%20");
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "contains/" + destinationNameJSON);
+
+        Boolean contains = new Gson().fromJson(response.body(), Boolean.class);
+
+        return contains;
     }
 
     /**
@@ -210,7 +172,7 @@ public class Client {
 
         String destinationJSON = gson.toJson(destination);
 
-        this.post(API_ADDRESS + "add", destinationJSON);
+        httpRequests.post(API_ADDRESS + "add", destinationJSON);
     }
 
     /**
@@ -230,7 +192,7 @@ public class Client {
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
 
         String fixedDestinationName = destinationName.replace(" ", "%20");
-        this.post(API_ADDRESS + "storeCurrent", fixedDestinationName);
+        httpRequests.post(API_ADDRESS + "storeCurrent", fixedDestinationName);
     }
 
     /**
@@ -247,7 +209,7 @@ public class Client {
     public void removeDestination(String destinationName)
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
 
-        this.post(API_ADDRESS + "remove", destinationName);
+        httpRequests.post(API_ADDRESS + "remove", destinationName);
     }
 
     /**
@@ -261,7 +223,7 @@ public class Client {
      */
     public void addActivity(String activity)
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
-        this.post(API_ADDRESS + "addActivity", activity);
+        httpRequests.post(API_ADDRESS + "addActivity", activity);
     }
 
     /**
@@ -275,7 +237,19 @@ public class Client {
      */
     public void removeActivity(String activity)
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
-        this.post(API_ADDRESS + "removeActivity", activity);
+        httpRequests.post(API_ADDRESS + "removeActivity", activity);
+    }
+
+    public List<String> getActivites()
+            throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
+
+        Gson gson = new Gson();
+
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "getActivites");
+
+        String[] activities = gson.fromJson(response.body(), String[].class);
+
+        return Arrays.asList(activities);
     }
 
     /**
@@ -290,7 +264,25 @@ public class Client {
     public void setRating(int starNumber)
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
         String starStr = String.valueOf(starNumber);
-        this.post(API_ADDRESS + "setRating", starStr);
+        httpRequests.post(API_ADDRESS + "setRating", starStr);
+    }
+
+    /**
+     * Get rating for chosen destination through server
+     * 
+     * @return int - rating
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws ServerException
+     */
+    public int getRating() throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
+
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "getRating");
+
+        int rating = Integer.parseInt(response.body());
+
+        return rating;
     }
 
     /**
@@ -304,7 +296,24 @@ public class Client {
      */
     public void setArrivalDate(String arrivalDate)
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
-        this.post(API_ADDRESS + "setArrivalDate", arrivalDate);
+        httpRequests.post(API_ADDRESS + "setArrivalDate", arrivalDate);
+    }
+
+    /**
+     * Get arrival date for chosen destination through server
+     * 
+     * @return arrival date
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws ServerException
+     */
+    public String getArrivalDate()
+            throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
+
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "getArrivalDate");
+
+        return response.body();
     }
 
     /**
@@ -318,7 +327,24 @@ public class Client {
      */
     public void setDepartureDate(String departureDate)
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
-        this.post(API_ADDRESS + "setDepartureDate", departureDate);
+        httpRequests.post(API_ADDRESS + "setDepartureDate", departureDate);
+    }
+
+    /**
+     * Get departure date for chosen destination through server
+     * 
+     * @return departure date
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws ServerException
+     */
+    public String getDepartureDate()
+            throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
+
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "getDepartureDate");
+
+        return response.body();
     }
 
     /**
@@ -330,9 +356,27 @@ public class Client {
      * @throws ExecutionException
      * @throws ServerException      if http request not successfull
      */
-    public void updateComment(final String comment)
+    public void updateComment(String comment)
             throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
-        this.post(API_ADDRESS + "updateComment", comment);
+
+        httpRequests.post(API_ADDRESS + "updateComment", comment);
+    }
+
+    /**
+     * Get comment for chosen destination through server
+     * 
+     * @return comment
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws ServerException
+     */
+    public String getComment()
+            throws URISyntaxException, InterruptedException, ExecutionException, ServerException {
+
+        HttpResponse<String> response = httpRequests.get(API_ADDRESS + "getComment");
+
+        return response.body();
     }
 
 }
