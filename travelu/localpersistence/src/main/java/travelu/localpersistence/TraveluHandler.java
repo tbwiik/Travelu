358cf3030729083399a40e44de10279cf76773a1
John Hausberg Anfindsen <johnhanf@stud.ntnu.no>
2022-11-21 21:21:30 +0100

    chore: make traveluhandler checkstyle-compliant

--- travelu/localpersistence/src/main/java/travelu/localpersistence/TraveluHandler.java
+++ travelu/localpersistence/src/main/java/travelu/localpersistence/TraveluHandler.java
@@ -10,30 +10,29 @@ import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import travelu.core.Destination;
 import travelu.core.DestinationList;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
- * Handles file reading and writing
+ * Handles file reading and writing.
  */
 public class TraveluHandler {
 
     /**
-     * Default filename for destination-list
+     * Default filename for destination-list.
      */
-    private final static String DEFAULT_FILENAME_DLIST = "DestinationList.json";
+    private static final String DEFAULT_FILENAME_DLIST = "DestinationList.json";
 
     /**
-     * Default filename for name of current destination
+     * Default filename for name of current destination.
      */
-    private final static String DEFAULT_FILENAME_CURRENTD = "CurrentDestinationName.json";
+    private static final String DEFAULT_FILENAME_CURRENTD = "CurrentDestinationName.json";
 
     /**
-     * Get file path for stored files
-     * 
+     * Get file path for stored files.
+     *
      * @param filename to add to path
      * @return the filepath for the given filename
      */
@@ -43,23 +42,23 @@ public class TraveluHandler {
     }
 
     /**
-     * Get file using {@link #getFilePath(String)}
-     * 
+     * Get file using {@link #getFilePath(String)}.
+     *
      * @param filename of wanted file
      * @return file if any
      */
-    private static File getFile(String filename) {
+    private static File getFile(final String filename) {
         return new File(getFilePath(filename));
     }
 
     /**
-     * Writes to given file in {@code JSON Format}using {@link Gson}
-     * 
+     * Writes to given file in {@code JSON Format}using {@link Gson}.
+     *
      * @param object   usually destination list
      * @param filename writing to
      * @throws IOException if I/O fail
      */
-    public static void writeJSON(Object object, String filename) throws IOException {
+    public static void writeJSON(final Object object, final String filename) throws IOException {
         GsonBuilder builder = new GsonBuilder();
         builder.setPrettyPrinting().serializeNulls();
         Gson gson = builder.create();
@@ -73,49 +72,45 @@ public class TraveluHandler {
 
     /**
      * Writes {@link DestinationList} to default file in {@code JSON Format}using
-     * {@code Gson}
-     * 
+     * {@code Gson}.
+     *
      * @param destinationList
      * @throws IOException if I/O fail
      */
-    public static void save(DestinationList destinationList) throws IOException {
+    public static void save(final DestinationList destinationList) throws IOException {
         writeJSON(destinationList, DEFAULT_FILENAME_DLIST);
     }
 
     /**
-     * Writes name of {@link Destination} to default file using {@code GSON}
-     * 
+     * Writes name of {@link Destination} to default file using {@code GSON}.
+     *
      * @param destinationName to write to file
      * @throws IOException if I/O fail
      */
-    public static void saveDestinationName(String destinationName) throws IOException {
+    public static void saveDestinationName(final String destinationName) throws IOException {
         writeJSON(destinationName, DEFAULT_FILENAME_CURRENTD);
     }
 
     /**
-     * Load destination-list from file
+     * Load destination-list from file.
      * <p>
      * If no file exists, or are corrupted: empty destination-list will be written
      * to new file
      * <p>
      * Field for filename input due to use in testing
-     * 
+     *
      * @param filename to read destination from
      * @return {@link DestinationList} - with no elements if blank, corrupted or no
      *         file
      * @throws IOException if I/O fail
      */
-    public static DestinationList readDestinationListJSON(String filename) throws IOException {
+    public static DestinationList readDestinationListJSON(final String filename) throws IOException {
 
         // Init fields
         Gson gson = new Gson();
         DestinationList destinationList;
         BufferedReader bufferedReader;
 
-        /**
-         * If no file exists, or format of data is corrupted, creates new file and
-         * return empty list
-         */
         try {
             // Read from file
             bufferedReader = new BufferedReader(new FileReader(getFile(filename), Charset.defaultCharset()));
@@ -124,6 +119,8 @@ public class TraveluHandler {
             destinationList = gson.fromJson(bufferedReader, DestinationList.class);
 
         } catch (FileNotFoundException e) {
+            // If no file exists, or format of data is corrupted, creates new file,
+            // write and return empty list
             destinationList = new DestinationList();
             writeJSON(destinationList, filename);
             return destinationList;
@@ -139,8 +136,8 @@ public class TraveluHandler {
 
     /**
      * Load destination from default file using
-     * {@link #readDestinationListJSON(String)}
-     * 
+     * {@link #readDestinationListJSON(String)}.
+     *
      * @return {@link DestinationList} - empty if failures
      * @throws IOException if I/O fail
      */
@@ -149,27 +146,23 @@ public class TraveluHandler {
     }
 
     /**
-     * Load current destination-name
+     * Load current destination-name.
      * <p>
      * If no file exists, or are corrupted: empty string will be written to new file
      * <p>
      * Field for filename input due to use in testing
-     * 
+     *
      * @param filename destination file
      * @return name of current destination - empty string if none
      * @throws IOException if I/O fail
      */
-    public static String readCurrentDestinationNameJSON(String filename) throws IOException {
+    public static String readCurrentDestinationNameJSON(final String filename) throws IOException {
 
         // Init fields
         Gson gson = new Gson();
         String currentDestinationName = "";
         BufferedReader bufferedReader;
 
-        /**
-         * If no file exists, or format of data is corrupted, creates new file and
-         * return empty list
-         */
         try {
             // Read from file
             bufferedReader = new BufferedReader(new FileReader(getFile(filename), Charset.defaultCharset()));
@@ -178,6 +171,7 @@ public class TraveluHandler {
             currentDestinationName = gson.fromJson(bufferedReader, String.class);
 
         } catch (FileNotFoundException e) {
+            // If no file exists, or format of data is corrupted, write empty string to file
             writeJSON("", filename);
             return currentDestinationName;
         }
@@ -187,8 +181,8 @@ public class TraveluHandler {
 
     /**
      * Load current destination-name from default file using
-     * {@link #readCurrentDestinationNameJSON(String)}
-     * 
+     * {@link #readCurrentDestinationNameJSON(String)}.
+     *
      * @return name of destination - empty string if failures
      * @throws IOException
      */
@@ -197,8 +191,8 @@ public class TraveluHandler {
     }
 
     /**
-     * Clear destination name from default file
-     * 
+     * Clear destination name from default file.
+     *
      * @throws IOException if I/O fail
      */
     public static void clearDestinationName() throws IOException {
@@ -206,10 +200,10 @@ public class TraveluHandler {
     }
 
     /**
-     * Clear destinations from default file
+     * Clear destinations from default file.
      * <p>
      * This is done by writing empty {@link DestinationList} to file
-     * 
+     *
      * @throws IOException if I/O fail
      */
     public static void clearDestinationList() throws IOException {
---
+++ travelu/localpersistence/src/main/java/travelu/localpersistence/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package for local persistence functionality of the Travelu application.
+ */
+package travelu.localpersistence;
